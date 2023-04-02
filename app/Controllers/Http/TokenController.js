d947b6ac842494b5990eaf9470587d04c49ece34
Azhar Ogi <azharogi@gmail.com>
2023-04-02 14:12:00 +0700

    remove credential

--- app/Controllers/Http/TokenController.js
+++ app/Controllers/Http/TokenController.js
@@ -1,9 +1,8 @@
 "use strict";
 const VoiceResponse = require("twilio").twiml.VoiceResponse;
 const AccessToken = require("twilio/lib/jwt/AccessToken");
-const accountSid = "AC6f6c2f0f7383a2921a645b77495209f2"; // Your Account SID from www.twilio.com/console
-const authToken = "3157fd60e3c0bba7516d901f0902de6c"; // Your Auth Token from www.twilio.com/console
-// const ChatGrant = AccessToken.ChatGrant;
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 const VoiceGrant = AccessToken.VoiceGrant;
 
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -12,34 +11,12 @@ const twilioApiSecret = process.env.TWILIO_API_SECRET;
 
 // Used specifically for creating Chat tokens
 const outgoingApplicationSid = process.env.TWILIO_VOICE_SERVICE_SID;
-const identity = "azharogi@gmail.com";
+const identity = process.env.TWILIO_EMAIL_IDENTITY;
 
 const client = require("twilio")(accountSid, authToken);
 
 class TokenController {
   async generate() {
-    // const message = await client.messages.create({
-    //   body:  "Hello bro",
-    //   to: '+6288989543401',
-    //   from: '+15673443210'
-    // })
-
-    // return { success: 1, data: {
-    //  message
-    // }};
-
-    // const call = await client.calls.create({
-    //   url: 'http://demo.twilio.com/docs/voice.xml',
-    //   to: "+6288989543401",
-    //   from: "+15673443210",
-    // });
-
-    // return { success: 1, data: {
-    //   call
-    // }};
-
-    // const call = await client.calls('CA9f6fa41e6e9cf5ad836937337164ec64').fetch();
-
     const voiceGrant = new VoiceGrant({
       outgoingApplicationSid: outgoingApplicationSid,
       incomingAllow: true, // Optional: add to allow incoming calls
@@ -60,7 +37,7 @@ class TokenController {
     try {
       const toNumberOrClientName = request.body.To;
       console.log(toNumberOrClientName, request.body);
-      const callerId = "+15673443210";
+      const callerId = process.env.TWILIO_CALLER_ID;
       let twiml = new VoiceResponse();
       if (toNumberOrClientName == callerId) {
         let dial = twiml.dial();
